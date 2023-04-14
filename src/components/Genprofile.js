59350a0f4124bd3560247913e8cd7a3466934759
Biswajit Nag <biswajitnag68@gmail.com>
2023-04-15 03:30:10 +0530

    bn 15-04-23 ch

--- src/components/Genprofile.js
+++ src/components/Genprofile.js
@@ -7,16 +7,25 @@ export default function Genprofile(){
         <>
         <section className="home">
         <div className="text">Profile</div>
-            <div className="profile-page">
-      <div className="profile-picture">
+        <div className="p-4">
+            <div className="profile-page my-3">
+              <div className="profile">
+      <div className="profile-picture ">
         <img src={profilePicture} alt="Profile" />
+        
       </div>
       <div className="profile-info">
-        <h2>Biswajit Nag</h2>
-        <p>hbckjndsankjs jbkdnksd kjsbknksc kjasdjksnjnc kbcsabjcbsjb kasbbsk
-            ncasknbckn jkskncskncnc kasnkcnscn ksnckndskncj ksbnckjs ck kajsnckj 
-            nckksdnknck njcsksnsn cnskcn sb cjjsnc nsnsckjdsjjjd 
-        </p>
+        <div className="profilevars rounded">Name<div className="profilevals">
+          Biswajit Nag
+          </div></div>
+          <div className="profilevars rounded">Mobile<div className="profilevals">
+          787288900
+          </div></div>
+          <div className="profilevars rounded">Email<div className="profilevals">
+          biswajit@gmail.com
+          </div></div>
+      </div>
+      </div>
       </div>
     </div>
     </section>
