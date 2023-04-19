8edb85bf36f42c21595f3c72fb4f67519f49a4ab
Biswajit Nag <biswajitnag68@gmail.com>
2023-04-19 22:02:55 +0530

    bn

--- src/components/Forgotpassword.js
+++ src/components/Forgotpassword.js
@@ -1,7 +1,11 @@
 import '../App.css';
+import {useState,useEffect} from 'react'
 import {Outlet, Link ,useNavigate} from "react-router-dom";
 export default function Forgotpassword(){
     const navigate=useNavigate();
+    const [alert,setalert]=useState();
+    var [alertmessage,setalm]=useState("Here is an alert");
+    const [credentials, setCredentials] = useState({user:"user", email: ""}) 
     var lst={
         display:"flex",
         justifyContent:"center",
@@ -15,19 +19,77 @@ export default function Forgotpassword(){
         textAlign:"center",
         padding:4
     };
+
+    useEffect(()=>{
+        if(localStorage.getItem("authtoken"))
+        navigate("../")
+    },[])
+    
+        //Forgotpassword api call
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            console.log(credentials);
+            const response = await fetch("https://room-rover-app-backend-mern.onrender.com/api/generateOTP", {
+                method: 'POST',
+                crossDomain: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: "application/json",
+                     "Access-Control-Allow-Origin": "*",
+    
+                },
+                
+                body: JSON.stringify({userSellerType:credentials.user, email:credentials.email})
+            });
+            const json = await response.json()
+            if (json.success){
+                // Save the auth token and redirect
+                localStorage.setItem('emailtoken', json.token); 
+                localStorage.setItem("usertype",credentials.user);
+                setalm(json.message)
+                setalert(true);
+                
+                
+    
+            }
+            else{
+                setalm(json.error)
+                setalert(true);
+            }
+            setTimeout(() => {
+                setalert(false);
+                if(json.success)
+                navigate("../")
+            }, 10000);
+        }
+    
+        const onchange = (e)=>{
+            setCredentials({...credentials, [e.target.name]: e.target.value})
+        }
+
     return(
         <>
         <section className="home">
         <div className="text">Email verify</div>
-    
-        <div><form >
+        {alert==true&&
+        <div className='d-flex justify-content-end aalert-container'>
+            <div className='d-flex alert rounded'><i className='bx bx-bell  mx-1'></i><span className='alerttext'>{alertmessage}</span></div>
+        </div>}
+        <div><form onSubmit={handleSubmit}>
         <div  style={lst}>
             <div className="wrap mx-4" style={nst}>
                 {/* <h2  style={st}>Log in</h2> */}
             <fieldset>
-            
+            <div className="form-floating mb-3">
+            <select class="form-select" aria-label="Default select example" onChange={onchange} name='user' value={credentials.user} id='floatinginput0'>
+                <option value="user">General User</option>
+                <option value="seller">Seller</option>
+            </select>
+            <label htmlFor="floatingInput0">User Type</label>
+            </div>
         <div className="form-floating mb-3">
-  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name='email' />
+  <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={onchange} value={credentials.email} name='email' />
   <label htmlFor="floatingInput">Email address</label>
 </div>
 <div className='d-grid gap-2 my-4'>
