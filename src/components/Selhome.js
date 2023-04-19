d0f30fd2333fb7d5182223be19b8036f4bd25748
RKS <ramkrishnasarkar12175@gmail.com>
2023-04-19 22:06:41 +0530

    table rks 19/04/23

--- src/components/Selhome.js
+++ src/components/Selhome.js
@@ -33,7 +33,41 @@ export default function Selhome() {
                     <label className=" lab-btn p-2  " >Contact No : 9932528811 </label>
                     <label className=" lab-btn p-2  " >No of Rooms : 10 </label>
                 </div>
+                
 
+                    <div className="table-responsive row">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Room No</th>
+                                    <th>Customer Name</th>
+                                    <th>Contact No</th>
+                                    <th>Price</th>
+                                    <th>Room status</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>101</td>
+                                    <td>Ramkrishna Sarkar</td>
+                                    <td>9923456789</td>
+                                    <td>8000</td>
+                                    <td>Booked</td>
+                                   
+                                </tr>
+                                <tr>
+                                    <td>102</td>
+                                    <td>Biswajit Nag</td>
+                                    <td>9923456789</td>
+                                    <td>8000</td>
+                                    <td>Booked</td>
+                                   
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                
 
             </section>
         </>
