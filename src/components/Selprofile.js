9d7ee8fc4c5ec573066b22e7721ab355d10335ab
RKS <ramkrishnasarkar12175@gmail.com>
2023-06-22 06:40:44 +0530

    12

--- src/components/Selprofile.js
+++ src/components/Selprofile.js
@@ -1,31 +1,18 @@
 
 import React, { useState } from 'react';
-import ToggleSwitch from '../components/ToggleSwitch';
+
 export default function Selprofile(){
 
-    const [items, setItems] = useState([
-        { id: 'item1', isOn: true },
-        { id: 'item2', isOn: false },
-        { id: 'item3', isOn: false },
-      ]);
-      setItems([
-        { id: 'item1', isOn: true },
-        { id: 'item2', isOn: false },
-        { id: 'item3', isOn: false },
-      ]);
+    
     return(
         <>
         <section className="home">
 
         <div>
             
-       
-           
-            {items.map((item) => (
-          <ToggleSwitch key={item.id} id={item.id} isOn={item.isOn} />
-        ))}
-         
-            </div>
+       hi
+        
+        </div>
         </section>
        
         </>
